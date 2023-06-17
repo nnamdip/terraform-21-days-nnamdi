6d921bbfa53d3db2fdbc9f2be0d3d5f81af6425c
Nnamdi <nnamdi.zobeashia@primespectrum.com>
2023-06-17 12:23:33 +0100

    deleted item

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired AWS region
+  region = "us-east-1" # Replace with your desired AWS region
 }
 
 resource "aws_vpc" "example_vpc" {
-  cidr_block = "10.0.0.0/16"  # Replace with your desired VPC CIDR block
+  cidr_block = "10.0.0.0/16" # Replace with your desired VPC CIDR block
 
   tags = {
     Name = "example-vpc"
@@ -12,7 +12,7 @@ resource "aws_vpc" "example_vpc" {
 
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.example_vpc.id
-  cidr_block              = "10.0.1.0/24"  # Replace with your desired public subnet CIDR block
+  cidr_block              = "10.0.1.0/24" # Replace with your desired public subnet CIDR block
   availability_zone       = "us-east-1a"  # Replace with your desired availability zone
   map_public_ip_on_launch = true
 
@@ -23,7 +23,7 @@ resource "aws_subnet" "public_subnet_1" {
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.example_vpc.id
-  cidr_block              = "10.0.2.0/24"  # Replace with your desired public subnet CIDR block
+  cidr_block              = "10.0.2.0/24" # Replace with your desired public subnet CIDR block
   availability_zone       = "us-east-1b"  # Replace with your desired availability zone
   map_public_ip_on_launch = true
 
@@ -33,9 +33,9 @@ resource "aws_subnet" "public_subnet_2" {
 }
 
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id                  = aws_vpc.example_vpc.id
-  cidr_block              = "10.0.3.0/24"  # Replace with your desired private subnet CIDR block
-  availability_zone       = "us-east-1a"  # Replace with your desired availability zone
+  vpc_id            = aws_vpc.example_vpc.id
+  cidr_block        = "10.0.3.0/24" # Replace with your desired private subnet CIDR block
+  availability_zone = "us-east-1a"  # Replace with your desired availability zone
 
   tags = {
     Name = "private-subnet-1"
@@ -43,9 +43,9 @@ resource "aws_subnet" "private_subnet_1" {
 }
 
 resource "aws_subnet" "private_subnet_2" {
-  vpc_id                  = aws_vpc.example_vpc.id
-  cidr_block              = "10.0.4.0/24"  # Replace with your desired private subnet CIDR block
-  availability_zone       = "us-east-1b"  # Replace with your desired availability zone
+  vpc_id            = aws_vpc.example_vpc.id
+  cidr_block        = "10.0.4.0/24" # Replace with your desired private subnet CIDR block
+  availability_zone = "us-east-1b"  # Replace with your desired availability zone
 
   tags = {
     Name = "private-subnet-2"
